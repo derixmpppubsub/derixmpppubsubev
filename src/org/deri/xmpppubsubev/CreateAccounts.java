cd9818d4da84ac354acbf3183aad1e700af53dd5
janaya <julia.anaya@gmail.com>
2011-09-20 23:28:09 +0100

    Added created accounts using in-band registration from java

---
+++ src/org/deri/xmpppubsubev/CreateAccounts.java
@@ -0,0 +1,51 @@
+package org.deri.xmpppubsubev;
+
+import org.apache.log4j.Logger;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+
+public class CreateAccounts {
+    
+    public void main(String[] args) {
+        XMPPConnection connection;
+        String userName;
+        String password;
+        String domain="localhost";
+        int port=5222;
+
+        Logger logger = Logger.getLogger(CreateAccounts.class);
+        ConnectionConfiguration config = new ConnectionConfiguration(domain,port);
+        connection = new XMPPConnection(config);
+        try {
+            connection.connect();
+            //publishers
+            for (int i=1; i<=1000; i++) {
+                userName = "pub"+i;
+                password = userName+"pass";
+                try {
+                    connection.login(userName, password);
+                    logger.info("User " + userName + " logged in to the server " 
+                            + domain);
+                } catch(XMPPException e) {
+                        connection.getAccountManager().createAccount(userName, password);
+                }
+            }
+            //subscribers
+            for (int i=1; i<=1000; i++) {
+                userName = "sub"+i;
+                password = userName+"pass";
+                try {
+                    connection.login(userName, password);
+                    logger.info("User " + userName + " logged in to the server " 
+                            + domain);
+                } catch(XMPPException e) {
+                        connection.getAccountManager().createAccount(userName, password);
+                }
+            }
+        } catch (XMPPException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+    }
+}
